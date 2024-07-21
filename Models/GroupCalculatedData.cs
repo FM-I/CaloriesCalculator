d17cd01e12d6c080ea711b1c21b675c86a637757
FM-I <maxfilimonhyk@gmail.com>
2024-07-21 09:30:09 +0300

    Add total calories to group

--- Models/GroupCalculatedData.cs
+++ Models/GroupCalculatedData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace CaloriesCalculator.Models;
 
@@ -6,9 +7,17 @@ public class GroupCalculatedData : ObservableCollection<CalcucaltedTotalData>
 {
     public DateOnly Date { get; set; }
 
+    public double TotalCalories { get => Math.Round(this.Sum(x => x.TotalCalories), 2); }
+
     public GroupCalculatedData(DateTime date, List<CalcucaltedTotalData> data)
         : base(data)
     {
         Date = new DateOnly(date.Year, date.Month, date.Day);
+    }
+
+    protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+    {
+        base.OnCollectionChanged(e);
+        OnPropertyChanged(new(nameof(TotalCalories)));
     }
 }
