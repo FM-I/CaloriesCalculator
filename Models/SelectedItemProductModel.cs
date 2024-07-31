9733afe4b64895745ca4524ad6fefd300e1a01bc
FM-I <maxfilimonhyk@gmail.com>
2024-07-31 21:43:33 +0300

    add observe changing them application in product list

--- Models/SelectedItemProductModel.cs
+++ Models/SelectedItemProductModel.cs
@@ -7,6 +7,29 @@ namespace CaloriesCalculator.Models
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(SelectedColor))]
         private bool _isSelected;
-        public Color SelectedColor => IsSelected ? Colors.AliceBlue : Colors.White;
+        public Color SelectedColor { 
+            get 
+            {
+                if(_theme == AppTheme.Dark)
+                    return IsSelected? Color.FromHex("#a490e8") : Color.FromHex("#795ae6"); 
+                    
+                return IsSelected? Colors.AliceBlue: Colors.White; 
+            } 
+        }
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(SelectedColor))]
+        private AppTheme _theme;
+
+        public SelectedItemProductModel()
+        {
+            Theme = Application.Current.RequestedTheme;
+            Application.Current.RequestedThemeChanged += ThemeChanged;
+        }
+
+        private void ThemeChanged(object? sender, AppThemeChangedEventArgs e)
+        {
+            Theme = e.RequestedTheme;
+        }
     }
 }
